c3e7b1ccbc7f628817b842a80f9d001122b0912f
sushreeit <43670145+sushreeit@users.noreply.github.com>
2025-01-23 00:56:42 -0800

    commit

--- src/main/java/com/example/demo/CloudAppApplication.java
+++ src/main/java/com/example/demo/CloudAppApplication.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CloudAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudAppApplication.class, args);
 	}
 
+	@GetMapping(path="/hello")
+	public String greet() {
+		return "Welcome to my portal";
+	}
 }
